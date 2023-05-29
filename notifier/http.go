cda4e7b0fa576361a7296bf4eca9cb7227779991
Julien Levesy <jlevesy@gmail.com>
2023-05-29 17:52:09 +0200

    feat(notifier): add a timeout

--- notifier/http.go
+++ notifier/http.go
@@ -4,24 +4,31 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type httpNotifier struct {
 	method string
 	url    string
 
+	timeout time.Duration
+
 	httpClient *http.Client
 }
 
-func NewHTTP(url, method string) Notifier {
+func NewHTTP(url, method string, timeout time.Duration) Notifier {
 	return &httpNotifier{
 		url:        url,
 		method:     method,
+		timeout:    timeout,
 		httpClient: http.DefaultClient,
 	}
 }
 
 func (n *httpNotifier) Notify(ctx context.Context) error {
+	ctx, cancel := context.WithTimeout(ctx, n.timeout)
+	defer cancel()
+
 	req, err := http.NewRequestWithContext(ctx, n.method, n.url, http.NoBody)
 	if err != nil {
 		return err
